dd04f31b9dea3f01218937d7d5321309ddf19158
Rakuto-NOV <okudarakuto@gmail.com>
2022-06-11 15:26:52 +0900

    モデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
  validates :nickname, presence: true
  validates :last_name, presence: true
  validates :first_name, presence: true
  validates :last_name_kana, presence: true
  validates :first_name_kana, presence: true
  validates :birth_day, presence: true
+ validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Password No alphanumeric mixture"}
 
  has_many :products
  has_many :purchase_histories
