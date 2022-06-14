500330556126540cc6cde09a4203d12396b2652f
Rakuto-NOV <okudarakuto@gmail.com>
2022-06-14 16:56:50 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:edit, :show]
   def index
     @products = Product.all.order(created_at: :desc)
   end
@@ -17,10 +18,18 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :cost_id, :prefecture_id, :arrival_id, :price,
                                     :image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
