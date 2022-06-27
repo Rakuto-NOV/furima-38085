1f3837cbe351bc3076319d658c5561fc65170593
Rakuto-NOV <okudarakuto@gmail.com>
2022-06-27 18:02:51 +0900

    商品出品画面のエラーメッセージを日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,6 @@ class Item < ApplicationRecord
   validates :image, :name, :description, :category_id, :status_id, :cost_id, :prefecture_id, :arrival_id, :price,
             presence: true
   validates :category_id, :status_id, :cost_id, :prefecture_id, :arrival_id,
-            numericality: { other_than: 1, message: "can't be blank" }
+            numericality: { other_than: 1, message: "が未選択です" }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
